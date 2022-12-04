c6a177547f5cbec9f659af6e801877dfb8394699
manosakpujiha <uwmano@yahoo.com>
2022-12-03 22:37:51 -0500

    fixData

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
-
 def no_c(my_string):
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    my_string_list = list(my_string)
+    index_count = 0
+    for index in my_string_list:
+        if index == 'c' or index == 'C':
+            my_string_list[index_count] = ""
+        index_count += 1
+    return "".join(my_string_list)
