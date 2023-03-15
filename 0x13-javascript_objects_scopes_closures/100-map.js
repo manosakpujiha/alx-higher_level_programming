bcc0a6e2bf8723e265a87008e6bfe27a59e4364a
manosakpujiha <manos.akpujiha@gmail.com>
2023-03-14 19:43:32 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-/* import a list */
-const lista = require('./100-data').list;
 
-const mapeado = lista.map((x, index) => {
-  return x * index;
-});
-console.log(lista);
-console.log(mapeado);
+const list = require('./100-data').list;
+
+console.log(list);
+console.log(list.map((x, i) => x * i));
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
-const newDict = {};
 
-for (const key in dict) {
-  if (typeof (newDict[dict[key]]) === 'undefined') {
-    newDict[dict[key]] = [];
-  }
-  newDict[dict[key]].push(key);
+const dict = require('./101-data').dict;
+const output = {};
+
+for (const entry in dict) {
+  if (!output[dict[entry]]) output[dict[entry]] = [];
+  output[dict[entry]].push(entry);
 }
 
-console.log(newDict);
+console.log(output);
